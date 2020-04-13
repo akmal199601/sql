d808e75cfac24bd7c32816c91a0f6d28118851f8
akmal199601 <akmal199601@mail.ru>
2020-04-13 18:32:47 +0500

    sqll

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.ConstrainedExecution;
+using System;
+
+namespace hw5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(akmal is the best);
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+    class akmal
+    {
+        public int id { get; set; }
+        public string LastName { get; set;}
+        public string FirstName {get;set;}
+        public string MiddleName {get;set;}
+    }
+}
